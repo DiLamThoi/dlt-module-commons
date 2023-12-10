6be0238e2d0e3f3a8854fc8183b811836c2102b3
TruongNBN <truongnbn.main@gmail.com>
2023-12-10 21:58:27 +0700

    add logout feature

--- dlt-components/components/sidebar/SideBarContainer.jsx
+++ dlt-components/components/sidebar/SideBarContainer.jsx
@@ -1,8 +1,20 @@
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
 import SideBarView from './SideBarView';
+import { useSignOut } from 'react-auth-kit';
+import { useNavigate } from 'react-router-dom';
 
-const mapStateToProps = () => ({});
+const SideBarContainer = (props) => {
+    const signOut = useSignOut();
+    const navigate = useNavigate();
 
-const SideBarContainer = connect(mapStateToProps, null)(SideBarView);
+    const onLogout = useCallback(() => {
+        signOut();
+        navigate('/login');
+    }, [navigate, signOut]);
+
+    return (
+        <SideBarView {...props} logout={onLogout} />
+    );
+};
 
 export default SideBarContainer;
--- dlt-components/components/sidebar/SideBarView.jsx
+++ dlt-components/components/sidebar/SideBarView.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Menu, theme } from 'antd';
+import { Button, Menu, theme } from 'antd';
+import { LogoutOutlined } from '@ant-design/icons';
+import { noop } from 'lodash/util';
 
 const SideBarView = (props) => {
+    const { logout } = props;
     const { token } = theme.useToken();
 
     const menuItems = [
@@ -13,16 +17,36 @@ const SideBarView = (props) => {
     ];
 
     return (
-        <Menu mode="vertical" theme="light" style={{ padding: '24px 8px', gap: 8 }}>
-            {menuItems.map((item) => (
-                <Menu.Item key={item.key}>
-                    <Link to={item.path}>
-                        <span style={{ color: token.colorTextSecondary, fontWeight: token.fontWeightStrong }}>{item.title}</span>
-                    </Link>
-                </Menu.Item>
-            ))}
-        </Menu>
+        <div style={{ padding: '24px 8px', display: 'flex', flexDirection: 'column', height: '100%' }}>
+            <Menu mode="vertical" theme="light" style={{ flex: 1, borderWidth: 0, gap: 8 }}>
+                {menuItems.map((item) => (
+                    <Menu.Item key={item.key}>
+                        <Link to={item.path}>
+                            <span style={{ color: token.colorTextSecondary, fontWeight: token.fontWeightStrong }}>{item.title}</span>
+                        </Link>
+                    </Menu.Item>
+                ))}
+            </Menu>
+            <Button
+                type="text"
+                onClick={logout}
+                icon={<LogoutOutlined />}
+                danger
+                ghost
+                style={{ fontWeight: token.fontWeightStrong }}
+            >
+                Đăng xuất
+            </Button>
+        </div>
     );
+};
+
+SideBarView.propTypes = {
+    logout: PropTypes.func,
+};
+
+SideBarView.defaultProps = {
+    logout: noop,
 };
 
 export default SideBarView;
